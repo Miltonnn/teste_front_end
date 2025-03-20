cf4d394c384e3a76f4fcf73dc65c89c86662a1e0
Miltonnn <juninhoeufrasio2017@hotmail.com>
2025-03-20 19:44:47 -0300

    Instalando as dependencias

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+  <div class="container text-center mt-5">
+    <h1 class="text-primary">Meu Projeto Vue</h1>
+    <button class="btn btn-success">Clique aqui</button>
+  </div>
+</template>
+
+<style scoped></style>
